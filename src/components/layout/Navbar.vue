df00b40bde92097d1ff9db372d30619d6c1176f0
Jacob Bryant <jacobryas4@gmail.com>
2019-06-21 15:08:41 -0400

    Login, Signup, Logout logic done

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,10 +2,10 @@
     <div class="navbar">
         <nav class="deep-purple darken-1">
             <div class="container">
-                <a href="" class="brand-logo left">Geo-Chatters</a>
+                <router-link :to="{ name: 'GMap'}">Geo-Chatters</router-link>
                 <ul class="right">
                     <li><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
-                    <li><a href="">Login</a></li>
+                    <li><router-link :to="{ name: 'Login' }">Login</router-link></li>
                     <li><a @click="logout">Logout</a></li>
                 </ul>
             </div>
@@ -26,7 +26,7 @@ export default {
     methods: {
         logout() {
             firebase.auth().signOut().then(() => {
-                this.$router.push({ name: 'Signup' })
+                this.$router.push({ name: 'Login' })
             })
         }
     }
